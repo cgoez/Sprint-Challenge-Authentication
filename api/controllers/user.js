2ad8457557121e46ac5537385a691978150542e0
cgoez <goez2001@gmail.com>
2018-05-18 10:04:16 -0700

    user.js require username and password

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,15 +7,21 @@ const createUser = (req, res) => {
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
   const user = new User({ username, password });
 
-  user
-    .save()
-    .then(newUser => {
-      console.log(newUser);
-      res.status(201).json(newUser);
-    })
-    .catch(error => {
-      res.status(500).json(error);
+  if (username && password) {
+    user
+      .save()
+      .then(newUser => {
+        console.log(newUser);
+        res.status(201).json(newUser);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(404).json({
+      error: "Please provide a USERNAME and PASSWORD."
     });
+  }
 };
 
 module.exports = {
