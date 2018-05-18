d6ba30c7c0b7421b4721354f395fc87e3fa7c9dd
cgoez <goez2001@gmail.com>
2018-05-18 09:51:37 -0700

    userModels pre save hook + pw check method

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -42,6 +42,13 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, function(error, response) {
+    if (error) {
+      return callBack(error);
+    } else {
+      callBack(null, response);
+    }
+  });
 };
 
 module.exports = mongoose.model("User", UserSchema);
