d7b65bcf6d487b0f5a33f9c86418bd2a69bcfc40
cgoez <goez2001@gmail.com>
2018-05-18 09:14:14 -0700

    userModels define user schema

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
@@ -8,9 +8,19 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre("save", function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
@@ -24,4 +34,4 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
